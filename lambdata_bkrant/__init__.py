3c1d576bea30cd70ba06239ccab3326a8beb0ef6
Boris Krant <gladiolusi70@gmail.com>
2019-05-29 15:30:37 -0400

    simple helper functions

--- lambdata_bkrant/__init__.py
+++ lambdata_bkrant/__init__.py
@@ -7,3 +7,38 @@ import pandas as pd
 import numpy as np
 
 TEST = pd.DataFrame(np.ones(10))
+
+class DataSci:
+    '''Contains useful functions for data science'''
+    def sqrt(n):
+        '''Takes any positive number and returns the square root '''
+        return n ** 0.5
+
+    def mean(nums):
+        '''Takes a list of numbers and returns the mean'''
+        return sum(nums) / len(nums)
+
+    def variance(nums, sample=False):
+        '''Takes a list of numbers and returns the variance of the list,
+        for sample variance use "sample=True"'''
+        if sample:
+             return sum((x - mean(nums)) ** 2 for x in nums) / (len(nums) - 1)
+        else:
+            return sum((x - mean(nums)) ** 2 for x in nums) / len(nums)
+
+    def stdev(nums):
+        '''Takes a list of numbers and returns the standard deviation'''
+        return sqrt(variance(nums))
+  
+    def zscore(n, nums):
+        '''Input number and list, returns zscore'''
+        return (n - mean(nums)) / stdev(nums)
+  
+    def find_outliers(nums, threshold=1):
+        '''input list of numbers returns list of outliers
+        , default threshold=1'''
+        outliers = []
+        for i in range(len(nums)):
+            if abs(zscore(nums[i],nums)) > threshold:
+                outliers.append(nums[i])
+        return outliers
---
+++ setup.py
@@ -0,0 +1,33 @@
+""" lambdata - a collection of Data Science helper functions
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas",
+    "category-encoders"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+
+setuptools.setup(
+    name="lambdata_bkrant",
+    version="0.0.2",
+    author="bkrant",
+    author_email="gladiolusi70@gmail.com",
+    description="A collection of Data Science Helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bkrant/lambdata",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
